bd440feae9f5e81f8ad62c9cc50a47803a82fe0f
Claudia Toh Yi An <Claudia@claudias-macbook-pro-2.local>
2016-03-31 07:52:00 +0800

    Initial Commit

---
+++ CurrencyConverter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CurrencyConverter
+//
+//  Created by Claudia Toh Yi An on 31/3/16.
+//  Copyright © 2016 Claudia Toh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CurrencyConverter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CurrencyConverter
+//
+//  Created by Claudia Toh Yi An on 31/3/16.
+//  Copyright © 2016 Claudia Toh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
